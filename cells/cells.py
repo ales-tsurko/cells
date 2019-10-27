b8fa93a91801b5bfb0c13c848ec5e98a97520ebb
Ales Tsurko <ales.tsurko@gmail.com>
2019-10-27 17:23:11 +0300

    Update project config

---
+++ cells/cells.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+def main():
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
