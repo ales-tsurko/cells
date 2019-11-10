d821cee9f242f1584c4fd52c046d057e6522e7dd
Ales Tsurko <ales.tsurko@gmail.com>
2019-11-10 17:01:35 +0300

    Add template info view to browser

--- cells/view/browser.py
+++ cells/view/browser.py
@@ -1,32 +1,104 @@
 from copy import deepcopy
 
-from PySide2.QtWidgets import (QListWidget, QListWidgetItem, QWidget,
-                               QFrame, QLabel, QVBoxLayout, QMenu,
-                               QAction)
 from PySide2.QtCore import Qt
-
-from cells.observation import Observation
+from PySide2.QtWidgets import (QAction, QFrame, QLabel, QListWidget,
+                               QListWidgetItem, QMenu, QTextEdit, QVBoxLayout,
+                               QWidget)
 
 from cells import events
 from cells.model import TrackTemplateManager
+from cells.observation import Observation
+
 from .dialogs import ConfirmationDialog
 from .track_editor import TrackEditor
 
 
-class Browser(Observation, QListWidget):
+class Browser(QWidget):
     def __init__(self, document, subject, powermode=False):
+        super().__init__()
+        self.document = document
+        self.subject = subject
+        self.powermode = powermode
+
+        self.setStyleSheet("border: 0;")
+        self.setMaximumWidth(250)
+        self.setMinimumWidth(165)
+
+        layout = QVBoxLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setAlignment(Qt.AlignTop)
+        self.setLayout(layout)
+
+        self._initList()
+        self._initDescription()
+
+    def _initList(self):
+        self.listView = BrowserList(self, self.document, self.subject,
+                                    self.powermode)
+        self.layout().addWidget(self.listView)
+
+    def _initDescription(self):
+        self.templateInfo = TemplateInfo()
+        self.layout().addWidget(self.templateInfo)
+
+    def onSelectionChanged(self, template):
+        self.templateInfo.setTemplate(template)
+
+    def delete(self):
+        self.listView.delete()
+
+    def closeEvent(self, e):
+        self.delete()
+
+
+class TemplateInfo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedHeight(200)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+        self.setLayout(layout)
+
+        self._initName()
+        self._initDescription()
+
+    def _initName(self):
+        self.name = QLabel()
+        self.name.setWordWrap(True)
+        self.layout().addWidget(self.name)
+
+    def _initDescription(self):
+        self.description = QTextEdit()
+        self.description.setReadOnly(True)
+        self.layout().addWidget(self.description)
+
+    def setTemplate(self, template):
+        if template is None:
+            self.name.setText("")
+            self.description.setText("")
+        else:
+            self.name.setText(template.backend_name)
+            self.description.setText(template.description)
+
+        self.name.adjustSize()
+        self.description.adjustSize()
+
+
+class BrowserList(Observation, QListWidget):
+    def __init__(self, delegate, document, subject, powermode=False):
         self.templateManager = TrackTemplateManager(document, subject)
+        self.delegate = delegate
         self.powermode = powermode
 
         Observation.__init__(self, subject)
         QListWidget.__init__(self)
-        self.templateEditor = TrackEditor(self.subject)
+        self.templateEditor = TrackEditor(self.subject, self.powermode)
         self.templateEditor.onTemplateUpdate = self.onTemplateUpdate
 
         self.setStyleSheet("border: 0;")
         self.setFrameStyle(QFrame.NoFrame | QFrame.Plain)
-        self.setMaximumWidth(250)
-        self.setMinimumWidth(165)
 
         self._initActions()
 
@@ -34,6 +106,8 @@ class Browser(Observation, QListWidget):
 
         self.add_responder(events.track.TrackTemplateSaved,
                            self.trackTemplateSavedResponder)
+
+        self.itemSelectionChanged.connect(self.onSelectionChanged)
 
     def _initActions(self):
         # as far as ownership of actions isn't transfered
@@ -52,9 +126,9 @@ class Browser(Observation, QListWidget):
             self._deleteAct.setShortcutContext(Qt.WidgetShortcut)
             self.addAction(self._deleteAct)
 
-            self._editAct = QAction("Edit")
-            self._editAct.triggered.connect(self.onTemplateEdit)
-            self.addAction(self._editAct)
+        self._editAct = QAction("Edit")
+        self._editAct.triggered.connect(self.onTemplateEdit)
+        self.addAction(self._editAct)
 
     def contextMenuEvent(self, event):
         menu = QMenu()
@@ -66,7 +140,8 @@ class Browser(Observation, QListWidget):
             return
 
         template = self.templateManager.templates[self.currentRow()]
-        self.notify(events.view.browser.TrackNewFromTemplate(deepcopy(template)))
+        self.notify(
+            events.view.browser.TrackNewFromTemplate(deepcopy(template)))
 
     def onTemplateDelete(self, e):
         if not self.currentItem():
@@ -78,6 +153,7 @@ class Browser(Observation, QListWidget):
         confirmation = ConfirmationDialog(
             "Delete Track Template",
             f"Do you really want to delete {name} track template?")
+
         if confirmation.exec_() == ConfirmationDialog.Yes:
             self.templateManager.delete_at(self.currentRow())
             self.takeItem(self.currentRow())
@@ -98,6 +174,13 @@ class Browser(Observation, QListWidget):
         itemWidget = self.itemWidget(self.currentItem())
         itemWidget.deserialize(template)
         self.templateManager.save(template, template.path, False)
+
+    def onSelectionChanged(self):
+        if not self.currentItem():
+            self.delegate.onSelectionChanged(None)
+
+        template = self.templateManager.templates[self.currentRow()]
+        self.delegate.onSelectionChanged(template)
 
     def addTemplate(self, template):
         item = QListWidgetItem()
@@ -147,6 +230,7 @@ class Item(Observation, QWidget):
     def shortenString(self, value):
         if len(value) <= self.maxLineLen:
             return value
+
         return value[:self.maxLineLen] + "..."
 
     def deserialize(self, template):
