7f9955b1450862a3cf69773815e9a5e5c6eaa613
AlesTsurko <ales.tsurko@gmail.com>
2019-12-23 17:15:00 +0000

    Deploying to gh-pages from master cc6d042c64bf722e2a118e2d4c056cf6330100a7

---
+++ precache-manifest.7de3de7b7eadf85077e82996b5fad05e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09891cf0665f32d762f7c4fa070c862b",
+    "url": "/cells/index.html"
+  },
+  {
+    "revision": "8854eb84fe1b9d23d056",
+    "url": "/cells/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a5f927240a44726db8f",
+    "url": "/cells/static/js/2.f5c7ea38.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cells/static/js/2.f5c7ea38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8854eb84fe1b9d23d056",
+    "url": "/cells/static/js/main.93b5439e.chunk.js"
+  },
+  {
+    "revision": "bf90376e4536da8f2083",
+    "url": "/cells/static/js/runtime-main.f0ea0471.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cells/static/media/logo.5d5d9eef.svg"
+  }
+]);
